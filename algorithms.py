840f7db8bba9029fe6b81ea77e0ee0ff73673923
Rudra Sarkar <da24d008@smail.iitm.ac.in>
2025-04-10 20:14:25 +0530

    Support for both the environments with Dueling DQN and MC REINFORCE

--- algorithms.py
+++ algorithms.py
@@ -70,38 +70,3 @@ class MonteCarloREINFORCE(nn.Module):
             return F.softmax(probs, dim = -1), value
 
 
-class CustomQN(nn.Module):
-      def __init__(self, state_dim:int, n_actions:int, use_max:bool):
-            super(DuelingDQN, self).__init__()
-
-            self.use_max = use_max
-
-            self.feature_embedding = nn.Sequential(
-                  nn.Linear(state_dim+1, 256),
-                  nn.ReLU(),
-                  nn.Linear(256, 128),
-                  nn.ReLU()
-            )
-
-            self.value = nn.Sequential(
-                  nn.Linear(128, 64),
-                  nn.ReLU(),
-                  nn.Linear(64, 1)
-            )
-
-            self.advantage = nn.Sequential(
-                  nn.Linear(128, 64),
-                  nn.ReLU(),
-                  nn.Linear(64, n_actions)
-            )
-            
-
-      def forward(self, state, reward):
-            x = torch.stack([torch.tensor(state), torch.tensor(reward)])
-            x = self.feature_embedding(x)
-            V = self.value(x)
-            A = self.advantage(x)
-            if self.use_max:
-                  return V + (A - A.max(dim = 1, keepdim = True).values)
-            
-            return V + (A - A.mean(dim = 1, keepdim = True))
